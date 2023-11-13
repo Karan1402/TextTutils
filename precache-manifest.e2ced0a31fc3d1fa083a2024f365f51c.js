9e9a1bc97ab4a6fbe734aaecc280c58ae43db360
karan kumar chauhan <95095124+Karan1402@users.noreply.github.com>
2023-11-13 17:41:34 +0530

    Updates

---
+++ precache-manifest.e2ced0a31fc3d1fa083a2024f365f51c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad87ef3f92bd6137d1b603321b41a5e",
+    "url": "/TextTutils/index.html"
+  },
+  {
+    "revision": "7560521a75d7fa971449",
+    "url": "/TextTutils/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/TextTutils/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextTutils/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/TextTutils/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "7560521a75d7fa971449",
+    "url": "/TextTutils/static/js/main.ec2c3a85.chunk.js"
+  },
+  {
+    "revision": "ce0a05610450ff6adc15",
+    "url": "/TextTutils/static/js/runtime-main.3f95b55c.js"
+  }
+]);
